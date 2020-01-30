a19def4f6738dec7ee523ec46c9e597aa59b9f2d
Songmao Li <li.songmao1609@gmail.com>
2020-01-30 12:20:49 +0000

    fix bugs in conv2d_autoencoder model

--- models/conv2d_autoencoder_separator.py
+++ models/conv2d_autoencoder_separator.py
@@ -1,55 +1,85 @@
 import tensorflow as tf
 from tensorflow import keras
+import tensorflow.keras.backend as K
 
 
 class AutoencoderConv2d:
     def __init__(self, freq_bins, time_frames):
         self.bins = freq_bins
         self.frames = time_frames
+        self.summary = dict()
+        self.model = None
 
-    def get_model(self):
+    def get_model(self, name='autoencoder_spectrogram'):
         """FCN design, autoencoder with concat skip connection"""
-        mix_input = keras.Input(shape=(self.frames, self.bins, 1), name='mix')
+        mix_input = keras.Input(shape=(self.bins, self.frames), name='mix')
+        reshaped_input = tf.expand_dims(mix_input, axis=3)
+
         # encoder
         # 1st conv + downsampling + batch normalization
-        conv1 = keras.layers.Conv2D(32, (31, 3), padding='same', activation='relu')(mix_input)
+        conv1 = keras.layers.Conv2D(32, (31, 3), padding='same', activation='relu')(reshaped_input)
         downsample1 = keras.layers.Conv2D(32, (31, 3), strides=(2, 2), padding='same',
                                           activation='relu', use_bias=False)(conv1)
-        x = keras.layers.BatchNormalization()(downsample1)
+        bn1 = keras.layers.BatchNormalization()(downsample1)
+
         # 2nd conv + downsampling + batch normalization
-        conv2 = keras.layers.Conv2D(64, (31, 3), padding='same', activation='relu')(x)
-        downsample1 = keras.layers.Conv2D(64, (31, 3), strides=(2, 2), padding='same',
+        conv2 = keras.layers.Conv2D(64, (31, 3), padding='same', activation='relu')(bn1)
+        downsample2 = keras.layers.Conv2D(64, (31, 3), strides=(2, 2), padding='same',
                                           activation='relu', use_bias=False)(conv2)
-        x = BatchNormalization()(downsample1)
+        bn2 = keras.layers.BatchNormalization()(downsample2)
+
         # 3rd conv + batch normalization
-        conv3 = keras.layers.Conv2D(128, (31, 3), padding='same', activation='relu', use_bias=False)(x)
-        x = keras.layers.BatchNormalization()(conv3)
+        conv3 = keras.layers.Conv2D(128, (31, 3), padding='same', activation='relu', use_bias=False)(bn2)
+        bn3 = keras.layers.BatchNormalization()(conv3)
+
         # decoder
         # 4th conv + upsampling + batch normalization
-        upsample1 = keras.layers.UpSampling2D((2, 2))(x)
-        x = keras.layers.Conv2D(64, (2, 2), activation='relu', use_bias=False)(x)
-        x = keras.layers.Concatenate([conv2, upsample1])
-        conv4 = keras.layers.Conv2D(64, (31, 3), padding='same', activatoin='relu')(x)
-        x = keras.layers.BatchNormalization()(conv4)
+        upsample1 = keras.layers.UpSampling2D((2, 2))(bn3)
+        up1_fixed_shape = keras.layers.Conv2D(64, (2, 1), activation='relu', use_bias=False)(upsample1)
+        conv4 = keras.layers.Conv2D(64, (31, 3), padding='same', activation='relu')(
+            keras.layers.concatenate([conv2, up1_fixed_shape]))
+        bn4 = keras.layers.BatchNormalization()(conv4)
+
         # 5th conv + upsampling + batch normalization
-        upsample2 = keras.layers.UpSampling2D((2, 2))(x)
-        x = keras.layers.Conv2D(32, (2, 2), activation='relu', use_bias=False)(x)
-        x = keras.layers.Concatenate([conv1, upsample2])
-        conv5 = keras.layers.Conv2D(32, (31, 3), padding='same', activatoin='relu')(x)
-        x = keras.layers.BatchNormalization()(conv5)
+        upsample2 = keras.layers.UpSampling2D((2, 2))(bn4)
+        up2_fixed_shape = keras.layers.Conv2D(32, (2, 2), activation='relu', use_bias=False)(upsample2)
+        conv5 = keras.layers.Conv2D(32, (31, 3), padding='same', activation='relu')(
+            keras.layers.concatenate([conv1, up2_fixed_shape]))
+        bn5 = keras.layers.BatchNormalization()(conv5)
+
         # output layers
-        x = keras.layers.Conv2D(32, (31, 3), padding='same', activation='relu')(x)
+        x = keras.layers.Conv2D(32, (31, 3), padding='same', activation='relu')(bn5)
         x = keras.layers.Conv2D(16, (31, 3), padding='same', activation='relu')(x)
         output = keras.layers.Conv2D(4, (31, 3), padding='same', activation='relu')(x)
 
-        vocals = output[:, :, :1]
-        bass = output[:, :, 1:2]
-        drums = output[:, :, 2:3]
-        other = output[:, :, 3:]
-        return vocals, bass, drums, other
+        vocals = keras.layers.Reshape((self.bins, self.frames), name='vocals')(output[:, :, :, :1])
+        bass = keras.layers.Reshape((self.bins, self.frames), name='bass')(output[:, :, :, 1:2])
+        drums = keras.layers.Reshape((self.bins, self.frames), name='drums')(output[:, :, :, 2:3])
+        other = keras.layers.Reshape((self.bins, self.frames), name='other')(output[:, :, :, 3:4])
+
+        self.model = keras.Model(inputs=[mix_input],
+                                 outputs=[vocals, bass, drums, other],
+                                 name=name)
+        return self.model
 
     def save_weights(self, path):
         pass
 
     def load_weights(self, path):
         pass
+
+    def model_summary(self):
+        if self.model is not None:
+            trainable_count = np.sum([K.count_params(w) for w in self.model.trainable_weights])
+            non_trainable_count = np.sum([K.count_params(w) for w in self.model.non_trainable_weights])
+            self.summary = {
+                'total_parameters': trainable_count + non_trainable_count,
+                'trainable_parameters': trainable_count,
+                'non_trainable_parameters': non_trainable_count}
+        else:
+            raise ValueError("no model has been built yet! call get_model() first!")
+
+    def __str__(self):
+        return self.summary
+
+    __repr__ = __str__

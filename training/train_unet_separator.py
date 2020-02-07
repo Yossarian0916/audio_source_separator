9dcf08253d848d8bb68f71bf93eadfc0f82fb1e7
Songmao Li <li.songmao1609@gmail.com>
2020-02-07 20:24:51 +0000

    remove batch normalization

--- training/train_unet_separator.py
+++ training/train_unet_separator.py
@@ -21,41 +21,23 @@ model = separator.get_model()
 model.summary()
 
 
-def decay(epoch, lr):
-    if lr < 0.001:
-        return lr
-    if epoch < 50:
-        return lr
-    elif epoch % 10 == 0:
-        return 0.1 * lr
-
-
-class ShowLearnintRate(tf.keras.callbacks.Callback):
-    def on_epoch_begin(self, epoch, logs=None):
-        if epoch % 10 == 0:
-            print('\nEpoch %03d: Learning rate is %6.4f.' %
-                  (epoch, self.model.optimizer.lr.numpy()))
-
-
 # callbacks: early-stopping, tensorboard
 log_dir = "./logs/unet_separator/" + datetime.now().strftime("%Y%m%d_%H%M%S")
 callbacks = [
     tf.keras.callbacks.EarlyStopping(
-        monitor='val_loss', min_delta=1e-3, verbose=True, patience=3),
+        monitor='val_loss', min_delta=1e-3, verbose=True, patience=20),
     tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1),
-    tf.keras.callbacks.LearningRateScheduler(decay),
-    ShowLearnintRate(),
 ]
 
 # BEGIN TRAINING
-model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.3, momentum=0.9, nesterov=True),
+model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.001, momentum=0.9, nesterov=True),
               loss={'vocals': tf.keras.losses.MeanSquaredError(),
                     'bass': tf.keras.losses.MeanSquaredError(),
                     'drums': tf.keras.losses.MeanSquaredError(),
                     'other': tf.keras.losses.MeanSquaredError()})
 
 history = model.fit(train_dataset,
-                    epochs=100,
+                    epochs=200,
                     validation_data=valid_dataset,
                     steps_per_epoch=train_data_size // BATCH_SIZE,
                     validation_steps=valid_data_size // BATCH_SIZE,
@@ -67,5 +49,5 @@ current_file_path = os.path.abspath(__file__)
 root = os.path.dirname(os.path.dirname(current_file_path))
 saved_model_dir = os.path.join(root, 'saved_model')
 saved_model_name = os.path.join(
-    saved_model_dir, 'unet_separator?time={}.h5'.format(date_time))
+    saved_model_dir, 'unet_separator_no_bn?time={}.h5'.format(date_time))
 model.save(saved_model_name)

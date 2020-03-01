fd069ffe5dd62f6312bacb6febc6396806e07027
Songmao Li <li.songmao1609@gmail.com>
2020-03-01 13:22:11 +0000

    update conv_resblock_denoising_unet model, reduce layers to 40 layers; save trained model

--- models/conv_resblock_denoising_unet.py
+++ models/conv_resblock_denoising_unet.py
@@ -8,7 +8,7 @@ class ConvResblockDenoisingUnet(SeparatorModel):
     def __init__(self, freq_bins, time_frames,
                  kernel_size=(3, 3),
                  kernel_initializer='he_normal',
-                 regularization=keras.regularizers.l1(0.001),
+                 regularization=keras.regularizers.l2(0.01),
                  name='conv_resblock_denoising_unet'):
         super(ConvResblockDenoisingUnet, self).__init__(freq_bins, time_frames, kernel_size, name)
         self.kernel_initializer = kernel_initializer
@@ -100,45 +100,55 @@ class ConvResblockDenoisingUnet(SeparatorModel):
         else:
             x = self.conv_block(input_tensor, filters, self.kernel_size, strides=(1, 1))
         x = self.identity_block(x, filters, self.kernel_size)
-        x = self.identity_block(x, filters, self.kernel_size)
+        # x = self.identity_block(x, filters, self.kernel_size)
         return x
 
     def resblock_unet(self, name=None):
         spectrogram = keras.Input(shape=(self.bins, self.frames, 1))
-        conv0 = keras.layers.Conv2D(4, (1, 1), padding='same',
-                                    activation='relu', use_bias=False,
+        conv1 = keras.layers.Conv2D(4, (1, 1), padding='same',
+                                    activation='relu', use_bias=True,
                                     kernel_initializer=self.kernel_initializer,
                                     kernel_regularizer=self.kernel_regularizer)(spectrogram)
-        block0 = self.residual_block(conv0, [1, 1, 4])
 
-        filters_set = [[8 << i, 8 << i, 16 << i] for i in range(4)]
+        filters_set = [[8 << i, 8 << i, 16 << i] for i in range(3)]
         # encoder
         # residual block, 1st downsampling
-        block1 = self.residual_block(block0, filters_set[0], downsample=True)
+        block1 = self.residual_block(conv1, filters_set[0], downsample=True)
         # residual block, 2nd downsampling
         block2 = self.residual_block(block1, filters_set[1], downsample=True)
         # residual block, 3rd downsampling
         block3 = self.residual_block(block2, filters_set[2], downsample=True)
 
         # latent tensor, compressed low dimensional features
-        latent = self.residual_block(block3, filters_set[3])
-        block4 = self.residual_block(latent, filters_set[3])
+        latent = keras.layers.BatchNormalization(axis=-1)(block3, training=True)
+        latent = keras.layers.LeakyReLU()(latent)
+        latent = keras.layers.Conv2D(128, self.kernel_size, padding='same',
+                                    activation=None, use_bias=False,
+                                    kernel_initializer=self.kernel_initializer,
+                                    kernel_regularizer=self.kernel_regularizer)(latent)
+        latent = keras.layers.BatchNormalization(axis=-1)(latent, training=True)
+        latent = keras.layers.LeakyReLU()(latent)
+        latent = keras.layers.Conv2D(128, self.kernel_size, padding='same',
+                                    activation=None, use_bias=False,
+                                    kernel_initializer=self.kernel_initializer,
+                                    kernel_regularizer=self.kernel_regularizer)(latent)
 
         # decoder
         # 1st upsampling + residual block
-        upsample1 = keras.layers.UpSampling2D((2, 2))(block4)
+        upsample1 = keras.layers.UpSampling2D((2, 2))(latent)
         block5 = self.residual_block(util.crop_and_concat(upsample1, block2), filters_set[2])
         # 2nd upsampling + residual block
         upsample2 = keras.layers.UpSampling2D((2, 2))(block5)
         block6 = self.residual_block(util.crop_and_concat(upsample2, block1), filters_set[1])
         # residual block + 3rd upsampling
         upsample3 = keras.layers.UpSampling2D((2, 2))(block6)
-        block7 = self.residual_block(util.crop_and_concat(upsample3, conv0), filters_set[0])
+        block7 = self.residual_block(util.crop_and_concat(upsample3, conv1), filters_set[0])
 
         # output layers
         output = keras.layers.BatchNormalization(axis=-1)(block7, training=True)
+        output = keras.layers.LeakyReLU()(output)
         output = keras.layers.Conv2D(1, (1, 1), padding='same',
-                                     activation='relu', use_bias=False,
+                                     activation=None, use_bias=False,
                                      kernel_initializer=self.kernel_initializer,
                                      kernel_regularizer=self.kernel_regularizer)(output)
         return keras.Model(inputs=[spectrogram], outputs=[output], name=name)
--- training/train_conv_resblock_denoising_unet.py
+++ training/train_conv_resblock_denoising_unet.py
@@ -5,11 +5,11 @@ from training.train_loop import TrainLoop
 
 separator = ConvResblockDenoisingUnet(1025, 100)
 optimizer = tf.keras.optimizers.Adam(lr=0.0001)
-training = TrainLoop(separator, batch_size=8, max_epochs=20, optimizer=optimizer)
+training = TrainLoop(separator, batch_size=16, max_epochs=20, optimizer=optimizer)
 
 
 if __name__ == '__main__':
-    history = training.compile_and_fit(validation_freq=1, verbose=2)
+    history = training.compile_and_fit(validation_freq=2, verbose=2)
     training.evaluate()
     training.save_model()
     training.save_weights()

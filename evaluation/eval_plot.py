43f29e5a657cc1a0e09d6a2835e30dd5a7b58add
Songmao Li <li.songmao1609@gmail.com>
2020-03-07 20:49:38 +0000

    generate boxplots of bss-eval metrics for saved models

--- evaluation/eval_plot.py
+++ evaluation/eval_plot.py
@@ -10,16 +10,37 @@ from utils import module_path
 mpl.use('agg')
 
 
-def boxplot_bss_eval_metrics(eval_results, save_name):
+def boxplot_bss_eval_metrics(eval_results, save_name, figure_title):
     # create a figure instance
     fig = plt.figure(1, figsize=(9, 6))
     # create an axes instance
-    ax = fig.add_subplot(111)
+    ax = fig.add_subplot()
+    ax.title.set_text(figure_title)
+    # create the boxplot
+    bp = ax.boxplot(eval_results, patch_artist=True)
+
+    # change outline color, fill color and linewidth of the boxes
+    for box in bp['boxes']:
+        # change outline color
+        box.set(color='#7570b3', linewidth=2)
+        # change fill color
+        box.set(facecolor='#1b9e77')
+    # change color and linewidth of the whiskers
+    for whisker in bp['whiskers']:
+        whisker.set(color='#7570b3', linewidth=2)
+    # change color and linewidth of the caps
+    for cap in bp['caps']:
+        cap.set(color='#7570b3', linewidth=2)
+    # change color and linewidth of the medians
+    for median in bp['medians']:
+        median.set(color='#b2df8a', linewidth=2)
+    # change the style of fliers and their fill
+    for flier in bp['fliers']:
+        flier.set(marker='o', color='#e7298a', alpha=0.5)
+
     ax.set_xticklabels(['vocal', 'bass', 'drums', 'other'])
     ax.get_xaxis().tick_bottom()
     ax.get_yaxis().tick_left()
-    # create the boxplot
-    bp = ax.boxplot(eval_results)
     # save the figure
     filename = save_name + '.png'
     fig.savefig(filename, bbox_inches='tight')
@@ -49,12 +70,14 @@ def print_metrics_mean_value(model_name):
     for metric in bss_metrics:
         mean_values[metric] = [value / num_json_files for value in sum_values[metric]]
     # print results
+    print(model_name)
     print('stem component order: [vocals, bass, drums, other]')
     for metric in bss_metrics:
-        print('mean value of ' + metric + ' ' + mean_values[metric])
+        print('mean value of ' + metric + ' ', mean_values[metric])
 
 
 def plot_model_eval_results(metric_name, model_name):
+    print('generating boxplot of metric: ', metric_name, ', model:', model_name)
     boxplot_data = [list(), list(), list(), list()]
     # get eval results path
     try:
@@ -63,7 +86,6 @@ def plot_model_eval_results(metric_name, model_name):
     except FileNotFoundError:
         print("Wrong file path or wrong model file name")
     # store bss-eval-metric data
-    num_json_files = len(os.listdir(eval_results_path))
     for file in os.listdir(eval_results_path):
         filename = os.path.join(eval_results_path, file)
         with open(filename, 'r', encoding='utf-8') as fd:
@@ -72,11 +94,14 @@ def plot_model_eval_results(metric_name, model_name):
                 boxplot_data[i].append(eval_metrics[metric_name][i])
     # output boxplot figure
     figure_name = metric_name + '_' + model_name
-    boxplot_bss_eval_metrics(boxplot_data, figure_name)
+    figure_path = os.path.join(evaluation_path, 'bss_eval_plots', figure_name)
+    figure_title = model_name.split('?')[0]
+    boxplot_bss_eval_metrics(boxplot_data, figure_path, figure_title)
 
 
 if __name__ == '__main__':
-    model_name = ''
-    print_metrics_mean_value(model_name)
-    for metric in ['sdr', 'sir', 'sar', 'nsdr']:
-        plot_model_eval_results(metric, model_name)
+    # model_name = 'conv_denoising_unet?time=20200307_1423.h5'
+    # model_name = 'conv_resblock_denoising_unet?time=20200307_1541.h5'
+    model_name = 'conv_encoder_denoising_decoder?time=20200227_0838_l2_weight_regularization.h5'
+    # print_metrics_mean_value(model_name)
+    plot_model_eval_results('nsdr', model_name)
